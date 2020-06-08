fa70ed8b3d8b89874ec7a99dca73ee1c5d122d6d
unknown <asdfqwerzxcv549@gmail.com>
2020-06-08 14:46:32 +0300

    3d commit

--- lab7/laba7_cpp/laba7_cpp.cpp
+++ lab7/laba7_cpp/laba7_cpp.cpp
@@ -1,20 +1,27 @@
-﻿// laba7_cpp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "List.h"
+using namespace std;
 
-#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "IS-92\nMinyailov Oleg\nLab 7" << endl;
+	List list = List();   //creating a list
+	list.Push(9);            //adding element in list(adding after first element) 
+	list.Push(5);
+	list.Push(6);
+	list.Push(2);
+	list.Push(10);
+	list.Push(7);
+
+	cout << "Our list: ";
+	list.output();            //outputing our list
+
+	cout << "Number of elements in list that divide 5:  " << list.Counting_numbers_dividing_5() << endl;    //output count of numbersthat divide 5 in a list
+
+	list.Remove_after();       //removing elems in list after max elem
+	cout << "Removing elements after max element" << endl;
+
+	cout << "Our list: ";
+	list.output();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
